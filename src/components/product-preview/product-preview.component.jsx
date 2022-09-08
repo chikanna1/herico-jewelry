a692933892f45ce0c323edfe149b954ad6a3bf39
Chika Nna <chika.ekene@gmail.com>
2022-09-08 12:26:14 -0400

    -

--- src/components/product-preview/product-preview.component.jsx
+++ src/components/product-preview/product-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./product-preview.styles.scss";
-import HoverImage from "react-hover-image";
+// import HoverImage from "react-hover-image";
 import { Link } from "react-router-dom";
 
 const ProductPreview = ({ item, size }) => (
