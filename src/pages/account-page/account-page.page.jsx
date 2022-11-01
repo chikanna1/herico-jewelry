ef8762d264cdb7365a431154c29a552606c32c9f
Chika Nna <chika.ekene@gmail.com>
2022-11-01 16:17:04 -0400

    Deploying Live Version

--- src/pages/account-page/account-page.page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./account.-page.styles.scss";
-
-const AccountPage = () => (
-  <div className="account-page-container">ACCOUNT PAGE</div>
-);
-
-export default AccountPage;
