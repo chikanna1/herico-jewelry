5a5cf37a8c3395e69f739c9db7bb1bc0b103cf1b
Chika Nna <chika.ekene@gmail.com>
2022-11-06 06:09:15 -0500

    Trying build on heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(express.json());
 
